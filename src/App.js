92f296d238da140054f6ad89d73c7192db72e98d
in-the-middle-of-nowhere <artemshum2033@mail.ru>
2020-04-03 15:49:11 +0300

    New one

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavbarComponent/>
-        <MapComponent/>
+        <InfoComponent/>
       </div>
     );
   }
--- src/components/InfoComponent.js
+++ src/components/InfoComponent.js
@@ -110,22 +110,22 @@ class InfoComponent extends Component{
                 <div className="info">
                     <div className="info--card card">
                         <div className="card--first-row">
-                            <p className="card--first-row__text">Баланс: { this.state.balance } ₽</p>
+                            <p className="card--first-row__text"><b>Баланс:</b> { this.state.balance } ₽</p>
                         </div>
                         <div className="card--second-row second-row">
                             <div className="second-row--first-col">
-                                <p className="second-row--first-col__label">Стоимость аккаунта</p>
+                                <p className="second-row--first-col__label">Стоимость<br/>аккаунта</p>
                                 <p className="second-row--first-col__text">{ this.state.account_price } ₽</p>
                             </div>
                             <div className="second-row--second-col">
-                                <p className="second-row--second-col__label">Доступно к покупке</p>
+                                <p className="second-row--second-col__label">Доступно к<br/>покупке</p>
                                 <p className="second-row--second-col__text">{ this.state.can_buy } шт.</p>
                             </div>
                         </div>
                     </div>
                     <div className="info--card card">
                         <div className="card--first-row">
-                            <p className="card--first-row__text">Аккаунты</p>
+                            <p className="card--first-row__text"><b>Аккаунты</b></p>
                         </div>
                         <div className="card--second-row second-row">
                             <div className="second-row--first-col">
@@ -140,7 +140,7 @@ class InfoComponent extends Component{
                     </div>
                     <div className="info--card card">
                         <div className="card--third">
-                            <p className="card--third__label">Общее количество виртуальных аккаунтов доступных к покупке</p>
+                            <p className="card--third__label"><b>Общее количество виртуальных аккаунтов<br/>доступных к покупке</b></p>
                             <p className="card--third__text">{ this.state.all_accounts_on_sms_acivate } шт.</p>
                         </div>
                     </div>
