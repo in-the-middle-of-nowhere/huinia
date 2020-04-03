a879eac635bdddca066bee62c287da45235a2fc6
dormantman <dormantman@ya.ru>
2020-04-03 16:39:34 +0300

    Added style

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -15,29 +15,67 @@ class MapComponent extends Component {
             circle: {
                 coords: [59.927171, 30.470315],
             },
-            markers: [{
-                geometry: [59.927171, 30.470315],
-                properties: {
-                    balloonContent: "<span>BMW 7x <br/> R283RR <br/> Черный\n" +
-                        "                        <br/> Василий Петрович <br/> Едет</span>"
-                },
-                modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
-            }, {
-                geometry: [59.927171, 30.42315],
-                properties: {
-                    balloonContent: "<span>BMW 7x <br/> R283RR <br/> Черный\n" +
-                        "                        <br/> Василий Петрович <br/> Едет</span>"
-                },
-                modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
-            }]
+            markers: [
+                this.newMarker({
+                    position: [59.927171, 30.470315],
+                    car: 'BMW 7x', number: 'R283RR',
+                    color: 'Черный', driver: 'Василий Петрович',
+                    status: 'waiting'
+                }),
+                this.newMarker({
+                    position: [59.927171, 30.450315],
+                    car: 'BMW 7x', number: 'R283RR',
+                    color: 'Черный', driver: 'Василий Петрович',
+                    status: 'driving'
+                })
+            ]
         };
     }
 
-    addMarker = (e) => {
-        const {markers} = this.state;
-        markers.length = 0;
-        markers.push(e.latlng);
-        this.setState({markers})
+    onMapClick(event) {
+        this.setState({
+            circle: {
+                coords: event.get('coords')
+            }
+        });
+    }
+
+    newMarker = (data) => {
+        const status = {
+            search: {
+                text: 'Поиск',
+                color: '#1771F1'
+            },
+            driving: {
+                text: 'Едет',
+                color: '#A400FF'
+            },
+            waiting: {
+                text: 'Ожидает',
+                color: '#F85C50'
+            }
+        };
+        const dataStatus = status[data.status];
+
+        return {
+            geometry: data.position,
+            properties: {
+                balloonContent: `<div style="width: 150px">
+                    <div style="display: flex; margin-bottom: 10px">
+                        <div style="padding: 5px; font-weight: bold">${data.car}</div>
+                        <div style="padding: 5px">${data.color}</div>
+                    </div>
+                    <div style="display: flex">
+                        <div style="padding: 4px; margin: 5px; margin-left: 0; color: ${dataStatus.color}">${dataStatus.text}</div>
+                        <div style="padding: 4px; margin: 5px; border: 1px solid #000; border-radius: 10px; width: 60px">
+                            ${data.number.slice(0, 1)} ${data.number.slice(1, 4)} ${data.number.slice(4)}
+                        </div>
+                    </div>
+                    <div style="margin-top: 10px; font-family: 'Roboto'">${data.driver}</div>
+                </div>`
+            },
+            modules: ['geoObject.addon.balloon']
+        }
     };
 
     componentDidMount() {
@@ -56,7 +94,8 @@ class MapComponent extends Component {
     render() {
         return (
             <YMaps>
-                <Map className="map" defaultState={{center: this.state.center, zoom: this.state.zoom}}>
+                <Map className="map" defaultState={{center: this.state.center, zoom: this.state.zoom}}
+                     onClick={this.onMapClick.bind(this)}>
                     {
                         this.state.markers.map(placeMark => {
                             return <Placemark {...placeMark} />
