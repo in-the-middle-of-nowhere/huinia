85ae6b1a802963daf9da50eff62a09edddcfc5ba
dormantman <dormantman@ya.ru>
2020-04-03 15:39:53 +0300

    Added yandex maps

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,78 +1,66 @@
 import React, {Component} from 'react';
-import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import icon_map from '../assets/map_icon.png';
+import {Map, Placemark, YMaps} from 'react-yandex-maps';
 import axios from 'axios';
-
 
 
 class MapComponent extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      greenIcon: {
-        lat: 59.940127,
-        lng: 30.251783,
-      },
-      zoom: 13,
-      orders: [],
-      markers: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            center: [59.927171, 30.470315],
+            zoom: 13,
+            orders: [],
+            markers: [{
+                geometry: [59.927171, 30.470315],
+                properties: {
+                    balloonContent: 'Тут пидорас'
+                },
+                modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+            }, {
+                geometry: [59.927171, 30.42315],
+                properties: {
+                    balloonContent: 'Тут пидорас'
+                },
+                modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+            }]
+        };
     }
-  }
 
-  addMarker = (e) => {
-    const {markers} = this.state;
-    markers.length = 0
-    markers.push(e.latlng)
-    this.setState({markers})
-  }
+    addMarker = (e) => {
+        const {markers} = this.state;
+        markers.length = 0;
+        markers.push(e.latlng);
+        this.setState({markers})
+    };
 
-  componentDidMount(){
-    axios.get('info/orders/?app_key=Zab+a-G$Z+NxEv4X%vUMAPnh?8-wE&ESdFz3GA&W5X=@QAVVBvmeWPz*-?JWF*et')
-      .then((response) => {
-        this.setState({
-          orders: response.data.data
-        });
-        console.log(this.state.orders);
-      })
-      .catch((error) => {
-          console.log(error);
-      })
-  }
+    componentDidMount() {
+        axios.get('info/orders/?app_key=Zab+a-G$Z+NxEv4X%vUMAPnh?8-wE&ESdFz3GA&W5X=@QAVVBvmeWPz*-?JWF*et')
+            .then((response) => {
+                this.setState({
+                    orders: response.data.data
+                });
+                console.log(this.state.orders);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
-  grenIcon = L.icon({
-    iconUrl: icon_map,
-    iconSize: [25, 40]
-  });
+    render() {
+        return (
+            <YMaps>
+                <Map className="map" defaultState={{center: this.state.center, zoom: this.state.zoom}}>
+                    {
+                        this.state.markers.map(placeMark => {
+                            return <Placemark {...placeMark} />
+                        })
+                    }
 
-
-  render(){
-    const positionGreenIcon = [this.state.greenIcon.lat, this.state.greenIcon.lng];
-
-    return (
-      <Map className="map" center={positionGreenIcon} zoom={this.state.zoom} onClick={this.addMarker}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {this.state.markers.map((position, idx) => 
-          <Marker key={`marker-${idx}`} position={position} icon={this.grenIcon}>
-            <Popup>
-              <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-            </Popup>
-          </Marker>
-        )}
-        {this.state.orders.map((element) => 
-          <Marker key={element.id} position={[element.start_latitude, element.start_longitude]}>
-            <Popup>
-              <span>{element.car} <br/> {element.number} <br/> {element.color} <br/> {element.driver} <br/> {element.status}</span>
-            </Popup>
-          </Marker>
-        )}
-      </Map>
-    );
-  }
+                </Map>
+            </YMaps>
+        );
+    }
 }
 
 export default MapComponent;
